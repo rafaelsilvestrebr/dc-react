20e6a574b39bf6cd8391183e36816e07f290730c
R. Silvestre Santos <r.silvestre.santos@avanade.com>
2018-04-10 15:44:52 -0300

    Rotas no react funcionando

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './containers/Header'
+import Main from './containers/Main'
 
-import { Routes } from './routes.js';
+const App = () => (
+  <div>
+    <Header />
+    <Main />
+  </div>
+)
 
-import { Link } from 'react-router-dom';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <hr />
-
-        <div className="title">
-          <h1>React Router demo</h1>
-          <Link to="/list"><button>Show the List</button></Link>
-        </div>
-      </div>
-    );
-  }
-}
-
-//export default App;
-module.exports = {
-  Title,
-  List
-};
+export default App
--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
+import React from 'react'
+import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import App from './App';
 
-
-// exemplo router https://medium.com/@pshrmn/a-simple-react-router-v4-tutorial-7f23ff27adf
-/*ReactDOM.render((
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  ), document.getElementById('root'))
-registerServiceWorker();*/
-ReactDOM.render(
-    <Router>
-        <div>
-          <Route exact path="/" component={Title} />
-          <Route path="/list" component={List} />
-        </div>
-    </Router>,
-    document.getElementById('app')
-  )
+render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
